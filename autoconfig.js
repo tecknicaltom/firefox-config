302ae8f386764ee7f265f3cdd558a99d3b336def
Tom Samstag <github@modtwo.com>
2019-02-12 21:47:42 -0800

    add browser.startup.page

--- autoconfig.js
+++ autoconfig.js
@@ -1,2 +1,5 @@
 // ctrl-tab brings up the preview dialog and switches to most recently used tab
 pref('browser.ctrlTab.previews', true);
+
+// start Firefox with the tabs from last time
+pref('browser.startup.page', 3);
