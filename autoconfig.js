c814c225eda8a3951650f2e3702c38615ca9e3a6
Tom Samstag <github@modtwo.com>
2019-02-12 23:38:18 -0800

    add extensions.pocket.enabled to kill pocket

--- autoconfig.js
+++ autoconfig.js
@@ -6,3 +6,6 @@ pref('browser.startup.page', 3);
 
 // restore the behavior of middle clicking anywhere will paste a URL to go to
 pref('middlemouse.contentLoadURL', true);
+
+// disable the Pocket extension
+pref('extensions.pocket.enabled', false);
